1e546b31ce626074d0401f3882114b3ec4429a49
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-04-25 16:26:05 +0100

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,18 @@
+import random
+
 def primary():
-  print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last = 13 # holds the highest index for the array
+  # last = len(quotes) - 1   # change the last variable to update automatically if quotes are added or removed from text file
+  rnd = random.randint(0, last)   # random number is stored in rnd
+  
+  print(quotes[rnd])   # prints random quote
+  # print(quotes[-1]) # prints last array value
 
 if __name__== "__main__":
   primary()
